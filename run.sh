ea9528050c2952f20b712a4b18c4c20fc1fdd8bf
CodeHz <CodeHz@outlook.com>
2018-05-29 19:56:55 +0800

    Add sleep

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 [ ! -e ./games/mcpeserver.sock ] && (./mcpeserver daemon &) &
+while [ ! -e ./games/mcpeserver.sock ]
+do
+  sleep 1
+done
 tail ./games/mcpeserver.log
 ./mcpeserver attach
